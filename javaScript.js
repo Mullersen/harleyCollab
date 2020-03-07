2c69bfa84fc74de0fb1f09f6d9cb23dfb84e2487
anconanda87 <hgmelgo@gmail.com>
2020-03-07 14:14:04 -0700

    added api

---
+++ javaScript.js
@@ -0,0 +1,46 @@
+document.body.style.height = '100vh';
+
+var title = document.getElementById('lyrics');
+title.style.color = 'yellow';
+title.style.top = 80 + "px";
+title.style.left = 1 + "vw";
+
+var singer = document.getElementById('singer');
+singer.style.color = 'black';
+singer.style.top = 90 + "px";
+singer.style.left = 1 + "vw";
+
+
+var btnOne = document.getElementById('one');
+btnOne.style.height = '5vh';
+btnOne.style.width = '10vw';
+btnOne.style.background = 'skyblue';
+btnOne.style.position = 'absolute';
+btnOne.style.top = 120 + "px";
+btnOne.style.left = 1 + "vw";
+
+var display = document.getElementById('lyricsBody');
+display.style.height = '50vw';
+display.style.width = '80vw';
+display.style.position = 'absolute';
+display.style.top = 200 + "px";
+display.style.left = 40 + "px";
+
+
+
+
+btnOne.addEventListener('click', function(event){
+    let ajax = new XMLHttpRequest();
+        ajax.onreadystatechange = function() {
+         if(ajax.readyState == 4 && ajax.status == 200) {
+             display.innerHTML = " ";
+             var lyricsJSON = JSON.parse(ajax.responseText); 
+                display.innerHTML += '<p>' + lyricsJSON.lyrics.replace(/(?:\r\n|\r|\n)/g, '</p><br>'); 
+                                                  
+                           
+         } 
+
+     }
+     ajax.open('GET', 'https://api.lyrics.ovh/v1/coldplay/yellow', true);
+     ajax.send();
+});
